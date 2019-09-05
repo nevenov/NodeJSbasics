1c141168e42f3732419a8775716561c0c948911e
Stoyan Nedev <nevenov@abv.bg>
2019-09-05 13:57:03 +0300

    EventEmiter addListener and emit

--- app_events.js
+++ app_events.js
@@ -3,10 +3,23 @@ const EventEmitter = require('events');
 const emmiter = new EventEmitter();
 
 // register a listener (same as emmiter.addListener)
-emmiter.on('messageLogged', function() {
-    console.log('Listener called');
+// emmiter.on('messageLogged', function(arg) { // by convention is arg, but could be e, or eventArg
+//     console.log('Listener called', arg);
+// });
+
+// this is the same as above, but it uses the ES6 simple syntax
+emmiter.on('messageLogged', (arg)=>{
+    console.log('Listener called', arg);
 });
+
 
 // emmit means: Making a noise, i.e signalling that an event has happened
 // we raised an event
-emmiter.emit('messageLogged');
+emmiter.emit('messageLogged', { id:1, url:'http://site.com'});
+
+// Raise: logging with (data:message)
+emmiter.on('logging', (arg)=>{
+    console.log(arg);
+});
+
+emmiter.emit('logging', {'message':'Successfully logged in'});
